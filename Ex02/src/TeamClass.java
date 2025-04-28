3f6af4054c65ae769b0d58d245ba6a677ff590ec
LSH <tmdghksgkqru@naver.com>
2025-04-28 15:37:15 +0900

    MyBranch

--- Ex02/src/TeamClass.java
+++ Ex02/src/TeamClass.java
@@ -4,7 +4,9 @@ public class TeamClass {
 	public static void main(String[] args) {
 		
 		System.out.println("팀장 첫번째");
-		System.out.println("팀원 1 두번째"); 
+		System.out.println("팀원 1 두번째");
+		System.out.println("나만의 작업공간 branch");
+		
 	}
 
 }
